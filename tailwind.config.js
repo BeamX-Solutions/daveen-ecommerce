c7ccbcc9de323eedc3c4dfa2edc5683677ea69c5
chimaobi-ibeh <ibehchimaobi98@gmail.com>
2025-09-30 15:18:55 +0100

    Coming soon I

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1A4D2E', // Deep green
+        secondary: '#FFB200', // Warm orange
+        accent: '#FFD93D', // Bright yellow
+        offwhite: '#F6F6F6',
+        lightgray: '#F2F2F2',
+      },
+      gradientColorStops: {
+        primary: '#1A4D2E',
+        secondary: '#FFB200',
+        accent: '#FFD93D',
+        offwhite: '#F6F6F6',
+        lightgray: '#F2F2F2',
+      },
+    },
   },
   plugins: [],
 };
